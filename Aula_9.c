1c4d2a30ff63a0b9db012491614d3474b43f4ac0
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-10-31 19:24:36 -0300

    teste porfavorfunciona

---
+++ Aula_9.c
@@ -0,0 +1,57 @@
+#include "stdio.h"
+#define ex02
+
+
+#ifdef ex01
+
+int main(){
+
+
+}
+
+#endif
+
+#ifdef ex02
+
+int main(){
+
+
+}
+
+#endif
+
+#ifdef ex03
+
+int main(){
+
+
+}
+
+#endif
+
+#ifdef ex04
+
+int main(){
+
+
+}
+
+#endif
+
+#ifdef ex05
+
+int main(){
+
+
+}
+
+#endif
+
+#ifdef ex06
+
+int main(){
+
+
+}
+
+#endif
