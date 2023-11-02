3b751704ffc0f91c0c7b1412a7a0735123034467
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-11-02 14:46:11 -0300

    Aula 9 sem exercicios feitos

--- Aula_9.c
+++ Aula_9.c
@@ -4,6 +4,14 @@
 
 #ifdef ex01
 
+/*1 - Escreva um programa que receba em uma funcao 2 strings de ate' 10 caracteres
+    passando como parametro ponteiro.
+    Os vetores devem ser declarados como variaveis LOCAIS na função main().
+    Escreva uma funcao para comparar as 2 strings. Passe como parametros para 
+    a funcao as 2 strings usando ponteiros e retorne como resultado se IGUAIS 1 
+    ou se DIFERENTES 0. Mostre o resultado no video na funcao main().
+*/
+
 int main(){
 
 
@@ -12,6 +20,14 @@ int main(){
 #endif
 
 #ifdef ex02
+
+/*
+2 - Escreva um programa que receba n valores via teclado e receba tambem a
+    operação a ser executada. Quando for digitado "=" o programa deve mostrar
+    o resultado acumulado dos n valores. As operações aritmeticas e a entrada
+    de dados devem ser funcoes que recebe os valores usando ponteiros.  
+    As variaveis sao LOCAIS na funcao main().
+*/
 
 int main(){
 
@@ -22,6 +38,17 @@ int main(){
 
 #ifdef ex03
 
+/*
+3 - Escreva um programa que receba uma letra via teclado usando ponteiro. Escreva 
+    uma funcao que pesquise se esta letra existe no vetor abaixo usando ponteiros. 
+    Imprima o resultado da pesquisa no video na funcao main(). Passe como informacao
+    para a funcao a letra digitada e o vetor usando ponteiros e faca a pesquisa 
+    usando ponteiros.(utilize o comando return). 
+    O vetor deve ser declarado como variavel LOCAL na funcao main().
+
+    vetor -> b,d,f,h,j,k,m,o,q,s,u,w,y
+*/
+
 int main(){
 
 
@@ -30,6 +57,21 @@ int main(){
 #endif
 
 #ifdef ex04
+
+/*
+4 - Escreva um programa que tem uma estrutura da dados com os membros abaixo.
+    A estrutura e' uma variavel LOCAL na funcao main(). Receba via teclado o
+    conteudo de cada um dos membros numa funcao e imprima-os no video no
+    seguinte formato(também numa função).
+
+    estrutura: char, int, long, float, double, unsigned char, unsigned int,
+    unsigned long
+
+            10        20        30        40        50        60        70
+    1234567890123456789012345678901234567890123456789012345678901234567890
+        char      int       long                float               double
+              unsigned char       unsigned int        unsigned long
+*/
 
 int main(){
 
@@ -40,6 +82,16 @@ int main(){
 
 #ifdef ex05
 
+/*
+5 - Escreva um programa com a estrutura abaixo. Defina um vetor de estruturas
+    de 4 elementos como variavel LOCAL na funcao main(). Receba os 4 registros
+    sequencialmente pelo teclado numa função e imprima todos os registros no
+    video em outra funcao. Faça um menu. Utilize ponteiros nas funcoes.
+    Coloque no menu a opção de sair tambem. Utilize o comando switch.
+    (vetor de estruturas)
+     estutura: nome, end, cidade, estado, cep
+*/
+
 int main(){
 
 
@@ -48,6 +100,12 @@ int main(){
 #endif
 
 #ifdef ex06
+
+/*
+6 - Acrescente ao menu do exercicio anterior as funcoes de procura, altera e
+    exclui um registro sempre usando ponteiros.
+*/
+
 
 int main(){
 
