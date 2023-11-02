891937eaf0d304dee7ea3ab067812820e1c1967b
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-11-02 14:43:08 -0300

    Exercício 4 e 5

--- Aula_8.c
+++ Aula_8.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
-#define ex03
+#define ex05
 
 #ifdef ex01
 
@@ -203,6 +203,17 @@ int main()
 
 int main()
 {
+    int n1, *pn1, soma = 0, *psoma, i = 0;
+    pn1 = &n1;
+    psoma = &soma;
+
+    do{
+    printf("Digite um numero inteiro:\n");
+    scanf("%d", pn1);
+    *pn1>=0 ? *psoma += *pn1, i++ : printf("Numero negativo digitado, encerrando o loop...\n");
+    }while(*pn1 >= 0);
+
+    printf("A media acumulada e: %d", soma/i);
 
     return 0;
 }
@@ -211,8 +222,53 @@ int main()
 
 #ifdef ex05
 
+/*
+5 - Com a estrutura abaixo, defina um vetor de estruturas de 4 elementos.
+    Receba os dados via teclado numa funcao usando ponteiros. Imprima os dados
+    recebidos no video numa usando ponteiros. Utilize um comando de loop. 
+    O vetor de estruturas de ser declarado como variavel global
+       estrutura: nome, end, cidade, estado, cep
+*/
+
+struct pessoa{
+
+    char nome[15];
+    char endereco[20];
+    char cidade[11];
+    char estado[14];
+    char cep[8];
+};
+
+typedef struct pessoa pessoa;
+
+pessoa cadastro[4];
+pessoa *ptrCadastro = &cadastro[0];
+
+
+void cadastrar(){
+
+int i = 0;
+
+    for(i = 0; i<4; i++)
+    {
+        printf("Digite o nome da pessoa %d:\n", i+1);
+        gets((ptrCadastro+i)-> nome);
+        printf("Digite o endereco da pessoa %d:\n", i+1);
+        gets((ptrCadastro+i)-> endereco);
+        printf("Digite a cidade da pessoa %d:\n", i+1);
+        gets((ptrCadastro+i)-> cidade);
+        printf("Digite o estado da pessoa %d:\n", i+1);
+        gets((ptrCadastro+i)-> estado);
+        printf("DIgite o CEP da pessoa %d: \n", i+1);
+        gets((ptrCadastro+i)-> cep);
+
+    }
+
+}
+
 int main()
 {
+    cadastrar();
 
     return 0;
 }
@@ -221,10 +277,103 @@ int main()
 
 #ifdef ex06
 
+/*
+6 - Escreva um programa com a estrutura abaixo. Receba 2 datas via teclado 
+    usando ponteiros. Calcule e exiba o numero de dias entre as duas datas
+    usando ponteiros. Utilize um vetor de estruturas.
+    	estrutura: dia, mes e ano
+*/
+struct data
+{
+    int dia, mes, ano;
+};
+
+typedef struct data data;
+
+int calculaDias(data *calendario)
+{
+    int diasPorMes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int dias1 = calendario[0].dia;
+    int dias2 = calendario[1].dia;
+
+
+    // Adicione os dias dos meses completos no primeiro ano
+    for (int i = 0; i < calendario[0].mes; i++)
+    {
+        dias1 += diasPorMes[i];
+    }
+
+    for(int i = 0; i < calendario[1].mes; i++)
+    {
+        dias2 += diasPorMes[i];
+    }
+
+    for (int ano = calendario[0].ano; ano < calendario[1].ano; ano++)
+    {
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))
+        {
+            dias2 += 366;
+        }
+        else
+        {
+            dias2 += 365;
+        }
+    }
+
+    return dias2-dias1;
+}
+
 int main()
 {
+    data *ptrCalendario;
+    data calendarioFunc[2];
+
+    ptrCalendario = &calendarioFunc[0];
+
+    char ch;
+
+    do
+    {
+        printf("Digite uma data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+0) -> dia);
+        setbuf(stdin,NULL);
+        printf("Digite uma data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+0) -> mes);
+        setbuf(stdin,NULL);
+        printf("Digite uma data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+0) -> ano);
+        setbuf(stdin,NULL);
+
+        printf("Digite outra data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+1) -> dia);
+        setbuf(stdin,NULL);
+        printf("Digite outra data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+1) -> mes);
+        setbuf(stdin,NULL);
+        printf("Digite outra data (dd MM AAAA): ");
+        scanf("%d",&(ptrCalendario+1) -> ano);
+        setbuf(stdin,NULL);
+
+        int diferenca = calculaDias(ptrCalendario);
+
+        if(diferenca>0)
+        {
+            printf("A diferenca entre as datas e de: %d dias\n", diferenca);
+        }
+        else if(diferenca == 0)
+        {
+            printf("A diferenca e 0\n");
+        }
+        else
+        {
+            printf("A primeira data deve ser maior que a segunda\n");
+        }
+        printf("Deseja encerrar o programa? (s/n): ");
+        scanf(" %c",&ch);
+        setbuf(stdin, NULL);
+    }
+    while(ch != 's' && ch != 'S');
 
     return 0;
 }
-
 #endif
