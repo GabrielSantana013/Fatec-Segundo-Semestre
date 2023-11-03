dd2fe142aa64a1fad8e5d31f43f85386a04fada7
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2023-11-02 22:56:58 -0300

    Exercicio 1 finalizado git push cd clear

--- Aula_9.c
+++ Aula_9.c
@@ -1,22 +1,68 @@
 #include "stdio.h"
-#define ex02
-
+#define ex01
 
 #ifdef ex01
 
 /*1 - Escreva um programa que receba em uma funcao 2 strings de ate' 10 caracteres
     passando como parametro ponteiro.
     Os vetores devem ser declarados como variaveis LOCAIS na função main().
-    Escreva uma funcao para comparar as 2 strings. Passe como parametros para 
-    a funcao as 2 strings usando ponteiros e retorne como resultado se IGUAIS 1 
+    Escreva uma funcao para comparar as 2 strings. Passe como parametros para
+    a funcao as 2 strings usando ponteiros e retorne como resultado se IGUAIS 1
     ou se DIFERENTES 0. Mostre o resultado no video na funcao main().
 */
 
-int main(){
+void recebeString(char *pStr1, char *pStr2)
+{
 
+    printf("Digite uma string:");
+    gets(pStr1);
+    printf("Digite outra string:");
+    gets(pStr2);
 
 }
 
+int comparaString(char *pStr1, char *pStr2)
+{
+   
+    for (int i = 0; *pStr1 != '\0' && *pStr2 != '\0'; i++)
+    {
+        pStr1++;
+        pStr2++;
+
+        if (*pStr1 != *pStr2)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+int main()
+{
+    char esc, str1[11], str2[11], *pStr1, *pStr2;
+    pStr1 = & str1[0];
+    pStr2 = & str2[0];
+    do
+    {
+        recebeString(pStr1, pStr2);
+        int retorno = comparaString(pStr1,pStr2);
+
+        if (retorno == 1)
+        {
+            printf("As string são iguais");
+        }
+        else
+        {
+            printf("As string são diferentes");
+        }
+
+        printf("\nDeseja continuar?\t(s/n)");
+        scanf("%c", &esc);
+        getchar();
+    } while (esc != 'n' && esc != 'N');
+    return 0;
+}
 #endif
 
 #ifdef ex02
@@ -25,13 +71,12 @@ int main(){
 2 - Escreva um programa que receba n valores via teclado e receba tambem a
     operação a ser executada. Quando for digitado "=" o programa deve mostrar
     o resultado acumulado dos n valores. As operações aritmeticas e a entrada
-    de dados devem ser funcoes que recebe os valores usando ponteiros.  
+    de dados devem ser funcoes que recebe os valores usando ponteiros.
     As variaveis sao LOCAIS na funcao main().
 */
 
-int main(){
-
-
+int main()
+{
 }
 
 #endif
@@ -39,19 +84,18 @@ int main(){
 #ifdef ex03
 
 /*
-3 - Escreva um programa que receba uma letra via teclado usando ponteiro. Escreva 
-    uma funcao que pesquise se esta letra existe no vetor abaixo usando ponteiros. 
+3 - Escreva um programa que receba uma letra via teclado usando ponteiro. Escreva
+    uma funcao que pesquise se esta letra existe no vetor abaixo usando ponteiros.
     Imprima o resultado da pesquisa no video na funcao main(). Passe como informacao
-    para a funcao a letra digitada e o vetor usando ponteiros e faca a pesquisa 
-    usando ponteiros.(utilize o comando return). 
+    para a funcao a letra digitada e o vetor usando ponteiros e faca a pesquisa
+    usando ponteiros.(utilize o comando return).
     O vetor deve ser declarado como variavel LOCAL na funcao main().
 
     vetor -> b,d,f,h,j,k,m,o,q,s,u,w,y
 */
 
-int main(){
-
-
+int main()
+{
 }
 
 #endif
@@ -73,9 +117,8 @@ int main(){
               unsigned char       unsigned int        unsigned long
 */
 
-int main(){
-
-
+int main()
+{
 }
 
 #endif
@@ -92,9 +135,8 @@ int main(){
      estutura: nome, end, cidade, estado, cep
 */
 
-int main(){
-
-
+int main()
+{
 }
 
 #endif
@@ -106,10 +148,8 @@ int main(){
     exclui um registro sempre usando ponteiros.
 */
 
-
-int main(){
-
-
+int main()
+{
 }
 
 #endif
